290b164af5085300dba752f1de70e531da94de60
Colton Bruni <42007138+ColtonDaCoder@users.noreply.github.com>
2019-11-26 18:10:18 -0600

    More custom loop integration in robot class

--- src/main/java/com/team364/frc2019/Poseidon.java
+++ src/main/java/com/team364/frc2019/Poseidon.java
@@ -11,7 +11,13 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+import java.util.Arrays;
+
+import com.team1323.lib.util.CrashTracker;
+import com.team1323.loops.Looper;
 import com.team364.frc2019.OI.*;
+import com.team364.frc2019.subsystems.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -23,6 +29,14 @@ import com.team364.frc2019.OI.*;
 public class Poseidon extends TimedRobot {
   public static DriverOI oi;
 
+  private Swerve swerve;
+  private Superstructure s;
+	private SubsystemManager subsystems;
+
+	private Looper enabledLooper = new Looper();
+	private Looper disabledLooper = new Looper();
+
+
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
 
@@ -33,6 +47,13 @@ public class Poseidon extends TimedRobot {
   @Override
   public void robotInit() {
     oi = new DriverOI();
+    s = Superstructure.getInstance();
+    swerve = Swerve.getInstance();
+    subsystems = new SubsystemManager(
+      Arrays.asList(swerve, s));
+
+    subsystems.registerEnabledLoops(enabledLooper);
+		subsystems.registerDisabledLoops(disabledLooper);
   }
 
   /**
@@ -47,51 +68,21 @@ public class Poseidon extends TimedRobot {
   public void robotPeriodic() {
   }
 
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   * You can use it to reset any subsystem information you want to clear when
-   * the robot is disabled.
-   */
-  @Override
-  public void disabledInit() {
-  }
-
   @Override
   public void disabledPeriodic() {
     Scheduler.getInstance().run();
   }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString code to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional commands to the
-   * chooser code above (like the commented example) or additional comparisons
-   * to the switch structure below with additional strings & commands.
-   */
+
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_chooser.getSelected();
 
-    /*
-     * String autoSelected = SmartDashboard.getString("Auto Selector",
-     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-     * = new MyAutoCommand(); break; case "Default Auto": default:
-     * autonomousCommand = new ExampleCommand(); break; }
-     */
-
-    // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.start();
     }
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
   @Override
   public void autonomousPeriodic() {
     Scheduler.getInstance().run();
@@ -99,26 +90,39 @@ public class Poseidon extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
+		try {
+			disabledLooper.stop();
+			enabledLooper.start();
+		} catch (Throwable t) {
+			CrashTracker.logThrowableCrash(t);
+			throw t;
+		}
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
   @Override
   public void teleopPeriodic() {
-    Scheduler.getInstance().run();
+    try {
+			//driver.update();
+		} catch (Throwable t) {
+			CrashTracker.logThrowableCrash(t);
+			throw t;
+		}
   }
 
-  /**
-   * This function is called periodically during test mode.
-   */
+  
+	@Override
+	public void disabledInit() {
+		try {
+			enabledLooper.stop();
+			subsystems.stop();
+			disabledLooper.start();
+		} catch (Throwable t) {
+			CrashTracker.logThrowableCrash(t);
+			throw t;
+		}
+	}
+
+ 
   @Override
   public void testPeriodic() {
   }
